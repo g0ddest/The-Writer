88417ed5829dc2d16c9f7a22f14d563c26bf3d52
Roman Proskuryakov <humbug@deeptown.org>
2014-01-21 07:46:39 +0400

    Adds an example of registration form
    Issue: #17

--- app/Writer.py
+++ app/Writer.py
@@ -142,32 +142,10 @@ def about():
 @app.route('/register/', methods=['GET', 'POST'])
 @guest_required
 def register():
-    if request.method == 'POST':
-        if request.form['username'] and request.form['name'] and request.form['password']:
-            mdfive = hashlib.md5()
-            mdfive.update(request.form['password'])
-            #TODO: Potential injection. Parse data, please
-            user = connection.User.find_one({'login': request.form['username']})
-            if user:
-                return 'Not a unique username'
-            else:
-                connection.User({
-                    'login': request.form['username'],
-                    'password': mdfive.hexdigest(),
-                    'name': request.form['name']
-                }).save()
-                return "User added!"
-        else:
-            return 'Please, fill all fields'
-    return '''
-        <form action="" method="post">
-            <p><input placeholder="Name" type=text name=name>
-            <p><input placeholder="Login" type=text name=username>
-            <p><input placeholder="Password" type=text name=password>
-            <p><input type=submit value=Register>
-        </form>
-    '''
-
+    register_form = forms.RegisterForm()
+    if register_form.validate_on_submit():
+        return "User added!"
+    return render_template('forms/register.html', form=register_form)
 
 @app.route('/me/')
 @login_required
--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import Form
 from wtforms import TextField, PasswordField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, EqualTo
 
 from app import connection
 
 import hashlib
+from pymongo.errors import DuplicateKeyError
 
 class LoginForm(Form):
     login = TextField('Login', validators=[DataRequired()])
@@ -29,3 +30,28 @@ class LoginForm(Form):
         else:
             self.login.errors.append('Try again')
             return False
+
+class RegisterForm(Form):
+    login = TextField('Login', validators=[DataRequired()])
+    name = TextField('Name', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm = PasswordField('Repeat Password', [
+        DataRequired(),
+        EqualTo('password', message='Passwords must match')
+    ])
+
+    def validate(self):
+        if not Form.validate(self):
+            return False
+        mdfive = hashlib.md5()
+        mdfive.update(self.password.data)
+        try:
+            connection.User({
+                'login': self.login.data,
+                'password': mdfive.hexdigest(),
+                'name': self.name.data
+            }).save()
+        except DuplicateKeyError as e:
+            self.login.errors.append('Not a unique login.')
+            return False
+        return True
